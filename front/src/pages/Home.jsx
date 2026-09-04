254c95b4a66c8fbbc36365b110626aa9f273f354
JangSeung <164428037+Jangseun@users.noreply.github.com>
2026-09-04 20:21:14 +0900

    타자연습 기본 기능 구현

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return(
         <div>
             <h1>Welcome to the Home Page</h1>
+            <h2>장승원 가위바위보 겁나 모다네</h2>
             
             <Link to="/typing/solo">Go to Typing Solo</Link>
             <br />
--- front/src/pages/TypingSolo.jsx
+++ front/src/pages/TypingSolo.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+
+
 
 const TypingSolo = () => {
+    const tText = "안녕하세요 좋은 날 이네요";
+
+    const [inputText, setInputText] = useState("");
+
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+
+        if (e.target.value === tText){
+            setInputText('');
+        }
+    }
+    
     return(
         <div>
-            <h1>Welcome to the Typing Solo Page</h1>   
+            <h1>Welcome to the Typing Solo Page</h1>
+
+            <p>{tText.split('').map((char, index) => {
+                let color = 'black';
+
+                if (inputText[index] === undefined) {
+                    color = 'gray';
+                }
+                else if ( inputText[index] === char) {
+                    color = 'green';
+                }
+                else{
+                    color = 'red';
+                }
+
+                return (
+                    <span key={index} style={{ color }}>
+                        {char}
+                    </span>
+                )
+            })}
+            </p>
+
+            <textarea
+                value={inputText}
+                onChange={handleChange}
+                placeholder="위 문장을 입력해 보세요"
+            />
+
+            <p>{inputText.length} / {tText.length}</p>
+            
         </div>
     )
 }
