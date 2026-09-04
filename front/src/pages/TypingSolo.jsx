b6ecbc9d8f739df1a76fc5fd7e1c050f0b09f511
JangSeung <164428037+Jangseun@users.noreply.github.com>
2026-09-04 14:08:17 +0900

    기본 구조 완성.
    Vite가 예전에 꼬였던 의존성을 캐시해놓아서.
    페이지가 뜨지 않았던 오류가 발생
    ```Remove-Item -Recurse -Force node_modules\.vite -ErrorAction SilentlyContinue
    npm run dev -- --force```
    
    이걸로 해결.

---
+++ front/src/pages/TypingSolo.jsx
@@ -0,0 +1,12 @@
+
+const TypingSolo = () => {
+    return(
+        <div>
+            <h1>Welcome to the Typing Solo Page</h1>   
+        </div>
+    )
+}
+
+
+
+export default TypingSolo
---
+++ front/src/pages/WritingSolo.jsx
@@ -0,0 +1,12 @@
+
+const WritingSolo = () => {
+    return(
+        <div>
+            <h1>Welcome to the WritingSolo Page</h1>   
+        </div>
+    )
+}
+
+
+
+export default WritingSolo
